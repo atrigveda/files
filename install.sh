fc8c6149dab834103366b5cb54e21b7e39c8c9f6
Ark <110242627+ArkArmAura@users.noreply.github.com>
2023-01-16 10:05:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 case "$1" in
   arch)
     $0 debian
+    sudo rm -r .git ;
     sudo cp -r ../files /srv/http/ ;
     cd /srv/http/files ;
     sudo rm -r install.sh uninstall.sh ;
     echo "install successful ." ;
     ;;
   debian)
+    sudo rm -r .git ;
     sudo cp -r ../files /var/www/html/ ;
     cd /var/www/html/files ;
     sudo rm -r install.sh uninstall.sh ; 
